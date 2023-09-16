e0467fe3709ca5ddef7d8c8faa3a0685a8a5ca69
Estelle Wolski <estelle.wolski@vercel.com>
2023-09-16 17:36:31 -0400

    [Layout] switch sections to normal layout to align sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="p-24">
       <ModeToggle />
       <About />
       <Education />

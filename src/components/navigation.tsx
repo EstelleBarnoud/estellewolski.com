ccda834772347647775b67c69094f5ce48f406f1
Estelle Wolski <estelle.wolski@vercel.com>
2023-09-17 16:06:23 -0400

    [Component] fix navigation button for small screens

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,5 +1,8 @@
+"use client"
 import Link from "next/link"
 import { ModeToggle } from "@/components/mode-toggle";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { Button } from "./ui/button";
 
 const sections = [
   {
@@ -37,38 +40,50 @@ export function Navigation() {
               My Portfolio
             </Link>
           </div>
-          <div className="flex md:hidden">
-            <button
-              aria-label="toggle menu"
-              className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
-              type="button"
-            >
-              <svg
-                className=" w-6 h-6 fill-current"
-                fill="none"
-                height="24"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <line x1="4" x2="20" y1="12" y2="12" />
-                <line x1="4" x2="20" y1="6" y2="6" />
-                <line x1="4" x2="20" y1="18" y2="18" />
-              </svg>
-            </button>
+          <div className="flex md:hidden space-x-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon"
+                  className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400">
+                  <svg
+                    className=" w-6 h-6 fill-current"
+                    fill="none"
+                    height="24"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <line x1="4" x2="20" y1="12" y2="12" />
+                    <line x1="4" x2="20" y1="6" y2="6" />
+                    <line x1="4" x2="20" y1="18" y2="18" />
+                  </svg>
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                { sections.map((section, index) => (
+                  <DropdownMenuItem>
+                    <Link className="text-gray-700 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-300" href={section.href} key={index}>
+                      {section.name}
+                    </Link>
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <ModeToggle />
           </div>
         </div>
-        <div className="space-x-4 md:space-x-8">
+        <div className="hidden md:block space-x-4 md:space-x-8">
           { sections.map((section, index) => (
             <Link className="text-gray-700 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-300" href={section.href} key={index}>
               {section.name}
             </Link>
           ))}
-        <ModeToggle />
+          <ModeToggle />
         </div>
       </nav>
     </section>

e2c50c6d52eae47c4a7e09dc425cc606da00c310
Estelle Wolski <estelle.wolski@vercel.com>
2023-09-17 00:01:46 -0400

    [Enhance] modularized skill items

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,6 +1,163 @@
 import Image from "next/image"
 import { Badge } from "@/components/ui/badge"
 
+const skills = [
+  {
+    title: "Analytics Engineering & Data Pipelines",
+    badges: [
+      {
+        name: "dbt",
+        image: "/images/dbt.png",
+      },
+      {
+        name: "BigQuery",
+        image: "/images/bigquery.png",
+      },
+      {
+        name: "Snowflake",
+        image: "/images/snowflake.png",
+      },
+      {
+        name: "Airflow",
+        image: "/images/airflow.png",
+      },
+      {
+        name: "Fivetran",
+        image: "/images/fivetran.png",
+      },
+      {
+        name: "Stitch",
+        image: "/images/stitch.png",
+      },
+    ],
+  },
+  {
+    title: "Data Visualization & Analysis",
+    badges: [
+      {
+        name: "Looker",
+        image: "/images/looker.png",
+      },
+      {
+        name: "Power BI",
+        image: "/images/powerbi.png",
+      },
+      {
+        name: "Tableau",
+        image: "/images/tableau.png",
+      },
+      {
+        name: "DOMO",
+        image: "/images/domo.png",
+      },
+      {
+        name: "Excel",
+        image: "/images/excel.png",
+      },
+      {
+        name: "VBA",
+        image: "/images/vba.png",
+      },
+    ],
+  },
+  {
+    title: "Data Sources",
+    badges: [
+      {
+        name: "PostgreSQL",
+        image: "/images/postgresql.png",
+      },
+      {
+        name: "MongoDB",
+        image: "/images/mongodb.png",
+      },
+      {
+        name: "Salesforce",
+        image: "/images/salesforce.png",
+      },
+      {
+        name: "HubSpot",
+        image: "/images/hubspot.webp",
+      },
+      {
+        name: "Zendesk",
+        image: "/images/zendesk.png",
+      },
+      {
+        name: "Pendo",
+        image: "/images/pendo.jpeg",
+      },
+      {
+        name: "Google Analytics",
+        image: "/images/google_analytics.webp",
+      },
+      {
+        name: "Airtable",
+        image: "/images/airtable.png",
+      },
+      {
+        name: "PubSub",
+        image: "/images/pubsub.svg",
+      },
+    ],
+  },
+  {
+    title: "Programming",
+    badges: [
+      {
+        name: "Python",
+        image: "/images/python.png",
+      },
+      {
+        name: "SQL",
+        image: "/images/sql.png",
+      },
+    ],
+  },
+  {
+    title: "DevOps",
+    badges: [
+      {
+        name: "git",
+        image: "/images/git.png",
+      },
+      {
+        name: "GitHub Actions",
+        image: "/images/github.png",
+      },
+      {
+        name: "GitLab CI",
+        image: "/images/gitlab.png",
+      },
+      {
+        name: "Docker",
+        image: "/images/docker.png",
+      },
+    ],
+  },
+  {
+    title: "Web Development",
+    badges: [
+      {
+        name: "HTML",
+        image: "/images/html.png",
+      },
+      {
+        name: "CSS",
+        image: "/images/css.webp",
+      },
+      {
+        name: "Javascript",
+        image: "/images/javascript.png",
+      },
+      {
+        name: "React",
+        image: "/images/react.png",
+      },
+    ],
+  },
+]
+
 export function Skills() {
 
   return (
@@ -10,172 +167,21 @@ export function Skills() {
         Skills
       </h2>
       <div className="grid grid-cols-3 gap-8">
-        <div>
-          <h3 className="text-xl text-black dark:text-white mb-4" >
-            Analytics Engineering &amp; Data Pipelines
-          </h3>
-          <div className="flex flex-wrap gap-2" >
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/dbt.png" alt="dbt logo" width={20} height={20} />
-              dbt
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/bigquery.png" alt="BigQuery logo" width={20} height={20} />
-              BigQuery
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/snowflake.png" alt="Snowflake logo" width={20} height={20} />
-              Snowflake
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/airflow.png" alt="Airflow logo" width={20} height={20} />
-              Airflow
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/fivetran.png" alt="Fivetran logo" width={20} height={20} />
-              Fivetran
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/stitch.png" alt="Stitch logo" width={20} height={20} />
-              Stitch
-            </Badge>
+        { skills.map((skill, index) => (
+          <div key={index}>
+            <h3 className="text-xl text-black dark:text-white mb-4" >
+              {skill.title}
+            </h3>
+            <div className="flex flex-wrap gap-2" >
+              { skill.badges.map((badge, index) => (
+                <Badge className="m-1" key={index} variant="outline">
+                  <Image className="m-2 rounded" src={badge.image} alt={`${badge.name} logo`} width={20} height={20} />
+                  {badge.name}
+                </Badge>
+              ))}
+            </div>
           </div>
-        </div>
-        <div>
-          <h3 className="text-xl text-black dark:text-white mb-4" >
-            Data Visualization &amp; Analysis
-          </h3>
-          <div className="flex flex-wrap gap-2" >
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/looker.png" alt="Looker logo" width={20} height={20} />
-              Looker
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/powerbi.png" alt="PowerBI logo" width={20} height={20} />
-              Power BI
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/tableau.png" alt="Tableau logo" width={20} height={20} />
-              Tableau
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/domo.png" alt="DOMO logo" width={20} height={20} />
-              DOMO
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/excel.png" alt="Excel logo" width={20} height={20} />
-              Excel
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/vba.png" alt="VBA logo" width={20} height={20} />
-              VBA
-            </Badge>
-          </div>
-        </div>
-        <div>
-          <h3 className="text-xl text-black dark:text-white mb-4" >
-            Data Sources
-          </h3>
-          <div className="flex flex-wrap gap-2" >
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/postgresql.png" alt="PostgreSQL logo" width={20} height={20} />
-              PostgreSQL
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/mongodb.png" alt="MongoDB logo" width={20} height={20} />
-              MongoDB
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/salesforce.png" alt="Salesforce logo" width={20} height={20} />
-              Salesforce
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/hubspot.webp" alt="HubSpot logo" width={20} height={20} />
-              HubSpot
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/zendesk.png" alt="Zendesk logo" width={20} height={20} />
-              Zendesk
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/pendo.jpeg" alt="Pendo logo" width={20} height={20} />
-              Pendo
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/google_analytics.webp" alt="Google Analytics logo" width={20} height={20} />
-              Google Analytics
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/airtable.png" alt="Airtable logo" width={20} height={20} />
-              AirTable
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/pubsub.svg" alt="PubSub logo" width={20} height={20} />
-              PubSub
-            </Badge>
-          </div>
-        </div>
-        <div>
-          <h3 className="text-xl text-black dark:text-white mb-4" >
-            Programming
-          </h3>
-          <div className="flex flex-wrap gap-2" >
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/python.png" alt="Python logo" width={20} height={20} />
-              Python
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/sql.png" alt="SQL logo" width={20} height={20} />
-              SQL
-            </Badge>
-          </div>
-        </div>
-        <div>
-          <h3 className="text-xl text-black dark:text-white mb-4" >
-            DevOps
-          </h3>
-          <div className="flex flex-wrap gap-2" >
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/git.png" alt="git logo" width={20} height={20} />
-              git
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/github.png" alt="GitHub logo" width={20} height={20} />
-              GitHub Actions
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/gitlab.png" alt="Gitlab logo" width={20} height={20} />
-              GitLab CI
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/docker.png" alt="Docker logo" width={20} height={20} />
-              Docker
-            </Badge>
-          </div>
-        </div>
-        <div>
-          <h3 className="text-xl text-black dark:text-white mb-4" >
-            Web Development
-          </h3>
-          <div className="flex flex-wrap gap-2" >
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/html.png" alt="HTML logo" width={20} height={20} />
-              HTML
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/css.webp" alt="CSS logo" width={20} height={20} />
-              CSS
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/javascript.png" alt="Javascript logo" width={20} height={20} />
-              Javascript
-            </Badge>
-            <Badge className="m-1" variant="outline">
-              <Image className="m-2 rounded" src="/images/react.png" alt="React logo" width={20} height={20} />
-              React
-            </Badge>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
 

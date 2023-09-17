18682f85f8e40f926e4b18abaaf66eebc2ffcfe4
Estelle Wolski <estelle.wolski@vercel.com>
2023-09-16 23:51:31 -0400

    [Enhance] modularized education items

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -1,4 +1,22 @@
 import Image from "next/image"
+import { Fragment } from "react"
+
+const education = [
+  {
+    logo: "/images/columbia.jpeg",
+    name: <Fragment>Columbia University</Fragment>,
+    degree: "Master of Science, Management Science & Engineering",
+    alt: "Columbia University Logo"
+  },
+  {
+    logo: "/images/telecom.webp",
+    name: <Fragment>Télécom Paris - <span className="font-extralight">
+      Institut Polytechnique de Paris
+    </span></Fragment>,
+    degree: "Master of Science (Diplôme d'ingénieur), Data Science",
+    alt: "Télécom Paris Logo"
+  },
+]
 
 export function Education() {
 
@@ -9,40 +27,25 @@ export function Education() {
         Education
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8" >
-        <div className="flex items-center" >
-          <Image
-            alt="Columbia University Logo"
-            className="w-24 h-24 object-cover rounded-lg"
-            height="100"
-            src="/images/columbia.jpeg"
-            width="100"
-          />
-          <div>
-            <h4 className="text-lg text-black dark:text-white ml-4" >
-              Columbia University
-            </h4>
-            <p className="ml-4 text-gray-600 dark:text-gray-400" >
-              Master of Science, Management Science & Engineering
-            </p>
+        {education.map((school, index) => (
+          <div className="flex items-center" key={index} >
+            <Image
+              alt={school.alt}
+              className="w-24 h-24 object-cover rounded-lg"
+              height="100"
+              src={school.logo}
+              width="100"
+            />
+            <div>
+              <h4 className="text-lg text-black dark:text-white ml-4" >
+                {school.name}
+              </h4>
+              <p className="ml-4 text-gray-600 dark:text-gray-400" >
+                {school.degree}
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex items-center" >
-          <Image
-            alt="Télécom Paris Logo"
-            className="w-24 h-24 object-cover rounded-lg"
-            height="100"
-            src="/images/telecom.webp"
-            width="100"
-          />
-          <div>
-            <h4 className="text-lg text-black dark:text-white ml-4" >
-              Télécom Paris - <span className="font-extralight">Institut Polytechnique de Paris</span>
-            </h4>
-            <p className="ml-4 text-gray-600 dark:text-gray-400" >
-              Master of Science (Diplôme d&apos;ingénieur), Data Science
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
 

a0be9e940ee0ff2eed97511509cab08d9ace710f
Estelle Wolski <estelle.wolski@vercel.com>
2023-09-16 21:03:26 -0400

    [Style] make all badge images corner rounded

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -16,27 +16,27 @@ export function Skills() {
           </h3>
           <div className="flex flex-wrap gap-2" >
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/dbt.png" alt="dbt logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/dbt.png" alt="dbt logo" width={20} height={20} />
               dbt
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/bigquery.png" alt="BigQuery logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/bigquery.png" alt="BigQuery logo" width={20} height={20} />
               BigQuery
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/snowflake.png" alt="Snowflake logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/snowflake.png" alt="Snowflake logo" width={20} height={20} />
               Snowflake
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/airflow.png" alt="Airflow logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/airflow.png" alt="Airflow logo" width={20} height={20} />
               Airflow
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/fivetran.png" alt="Fivetran logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/fivetran.png" alt="Fivetran logo" width={20} height={20} />
               Fivetran
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/stitch.png" alt="Stitch logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/stitch.png" alt="Stitch logo" width={20} height={20} />
               Stitch
             </Badge>
           </div>
@@ -47,15 +47,15 @@ export function Skills() {
           </h3>
           <div className="flex flex-wrap gap-2" >
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/looker.png" alt="Looker logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/looker.png" alt="Looker logo" width={20} height={20} />
               Looker
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/powerbi.png" alt="PowerBI logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/powerbi.png" alt="PowerBI logo" width={20} height={20} />
               Power BI
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/tableau.png" alt="Tableau logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/tableau.png" alt="Tableau logo" width={20} height={20} />
               Tableau
             </Badge>
             <Badge className="m-1" variant="outline">
@@ -63,11 +63,11 @@ export function Skills() {
               DOMO
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/excel.png" alt="Excel logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/excel.png" alt="Excel logo" width={20} height={20} />
               Excel
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/vba.png" alt="VBA logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/vba.png" alt="VBA logo" width={20} height={20} />
               VBA
             </Badge>
           </div>
@@ -78,39 +78,39 @@ export function Skills() {
           </h3>
           <div className="flex flex-wrap gap-2" >
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/postgresql.png" alt="PostgreSQL logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/postgresql.png" alt="PostgreSQL logo" width={20} height={20} />
               PostgreSQL
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/mongodb.png" alt="MongoDB logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/mongodb.png" alt="MongoDB logo" width={20} height={20} />
               MongoDB
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/salesforce.png" alt="Salesforce logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/salesforce.png" alt="Salesforce logo" width={20} height={20} />
               Salesforce
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/hubspot.webp" alt="HubSpot logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/hubspot.webp" alt="HubSpot logo" width={20} height={20} />
               HubSpot
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/zendesk.png" alt="Zendesk logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/zendesk.png" alt="Zendesk logo" width={20} height={20} />
               Zendesk
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/pendo.jpeg" alt="Pendo logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/pendo.jpeg" alt="Pendo logo" width={20} height={20} />
               Pendo
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/google_analytics.webp" alt="Google Analytics logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/google_analytics.webp" alt="Google Analytics logo" width={20} height={20} />
               Google Analytics
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/airtable.png" alt="Airtable logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/airtable.png" alt="Airtable logo" width={20} height={20} />
               AirTable
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/pubsub.svg" alt="PubSub logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/pubsub.svg" alt="PubSub logo" width={20} height={20} />
               PubSub
             </Badge>
           </div>
@@ -121,11 +121,11 @@ export function Skills() {
           </h3>
           <div className="flex flex-wrap gap-2" >
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/python.png" alt="Python logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/python.png" alt="Python logo" width={20} height={20} />
               Python
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/sql.png" alt="SQL logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/sql.png" alt="SQL logo" width={20} height={20} />
               SQL
             </Badge>
           </div>
@@ -136,19 +136,19 @@ export function Skills() {
           </h3>
           <div className="flex flex-wrap gap-2" >
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/git.png" alt="git logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/git.png" alt="git logo" width={20} height={20} />
               git
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/github.png" alt="GitHub logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/github.png" alt="GitHub logo" width={20} height={20} />
               GitHub Actions
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/gitlab.png" alt="Gitlab logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/gitlab.png" alt="Gitlab logo" width={20} height={20} />
               GitLab CI
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/docker.png" alt="Docker logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/docker.png" alt="Docker logo" width={20} height={20} />
               Docker
             </Badge>
           </div>
@@ -159,19 +159,19 @@ export function Skills() {
           </h3>
           <div className="flex flex-wrap gap-2" >
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/html.png" alt="HTML logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/html.png" alt="HTML logo" width={20} height={20} />
               HTML
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/css.webp" alt="CSS logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/css.webp" alt="CSS logo" width={20} height={20} />
               CSS
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/javascript.png" alt="Javascript logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/javascript.png" alt="Javascript logo" width={20} height={20} />
               Javascript
             </Badge>
             <Badge className="m-1" variant="outline">
-              <Image className="m-2" src="/images/react.png" alt="React logo" width={20} height={20} />
+              <Image className="m-2 rounded" src="/images/react.png" alt="React logo" width={20} height={20} />
               React
             </Badge>
           </div>
